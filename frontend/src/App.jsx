081c3e5d51b57eb4477dadf87ed4ba86bbabaf25
Samatara <samatarbulbula@gmail.com>
2024-05-06 22:57:47 +0200

    css
    fjernet den default css og importerte den i app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Teams from "./components/Teams";
 import Type from "./components/Type";
+import './App.css';
 
 export default function App() {
   return (
