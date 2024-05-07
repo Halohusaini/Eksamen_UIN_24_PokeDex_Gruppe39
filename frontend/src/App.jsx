722f442cef8a08e1e3a31971edb18b3048f2f80b
Halohusaini <hallohusaini56@gmail.com>
2024-05-07 22:19:13 +0200

    Importert den nye main.scss filen og slettet App.css
    Importert den nye main.scss filen og slettet App.css, for en ryddig mappe og fil struktur

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Teams from "./components/Teams";
 import Type from "./components/Type";
-import './App.css';
+import './styles/main.scss';
 import Pokemon from "./components/Pokemon";
 
 export default function App() {
