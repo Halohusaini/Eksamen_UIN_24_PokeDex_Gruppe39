5da0c0c6fff3553fba0b1a55a5d9f5d584066df0
Samatara <samatarbulbula@gmail.com>
2024-05-07 15:07:50 +0200

    type
    jeg har lag til routing for app.jsx og inn i type har jeg gjord slik at når man trykker på en type så henter api-en alle pokemonene som tilhører den

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Teams from "./components/Teams";
 import Type from "./components/Type";
 import './App.css';
+// import Pokemon from "./components/Pokemon";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="teams" element={<Teams />} />
         <Route path="type/:type" element={<Type />} />
+        {/* <Route path="pokemon/:name" element={<Pokemon />} /> */}
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/Type.jsx
+++ frontend/src/components/Type.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
+import React from 'react';
+
 
 
 export default function Type() {
@@ -39,16 +41,22 @@ export default function Type() {
     }
 
     return (
+        <main>
+            <section>
+                <button><Link to="/">Home</Link></button>
+            </section>
+
         <section>
             <h1>Pokemons of Type {type}</h1>
             <ul>
                 {pokemons.map(pokemon => (
                     <li key={pokemon.name}>
                         <Link to={`/pokemons/${pokemon.name}`}>{pokemon.name}</Link>
+                       
                     </li>
                 ))}
             </ul>
-            <Link to="/">Back to Home</Link>
         </section>
+        </main>
     )
 }
