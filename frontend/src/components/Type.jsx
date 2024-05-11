f602ccde7ae467f922052d2aea8c370232b385f1
Samatara <samatarbulbula@gmail.com>
2024-05-11 18:57:58 +0200

    Revert "scss og type"
    This reverts commit 4b6bd7a05217998f6c24fca6de02a6cd1aa245f6.

--- frontend/src/components/Type.jsx
+++ frontend/src/components/Type.jsx
@@ -40,49 +40,6 @@ export default function Type() {
         return <div>error:{error}</div>
     }
 
-    const getTypeColor = (type) => {
-        switch (type) {
-          case 'normal':
-            return '#dce1e6';
-          case 'fighting':
-            return '#f0c3d0';
-          case 'flying':
-            return '#dce4f4';
-          case 'poison':
-            return '#e6caf2';
-          case 'ground':
-            return '#f2c8b4';
-          case 'rock':
-            return '#f7eed4';
-          case 'bug':
-            return '#eaf4d3';
-          case 'ghost':
-            return '#d0d9f4';
-          case 'steel':
-            return '#d4edf7';
-          case 'fire':
-            return '#f4d5bf';
-          case 'water':
-            return '#cbe1f9';
-          case 'grass':
-            return '#cdf9c8';
-          case 'electric':
-            return '#f4eab8';
-          case 'psychic':
-            return '#f2c2c4';
-          case 'ice':
-            return '#d6f9f3';
-          case 'dragon':
-            return '#c6dff4';
-          case 'dark':
-            return '#d3cae3';
-          case 'fairy':
-            return '#f9dbf6';
-          default:
-            return '#ffffff'; // default color if the type is not listed
-        }
-      };
-
     return (
         <main>
             <section>
@@ -90,10 +47,11 @@ export default function Type() {
                 <h1>Pokemons of Type {type}</h1>
             </section>
 
-        <section className="typestyle" > 
+        <section className="typestyle">
+            
             <ul>
                 {pokemons.map(pokemon => (
-                    <li key={pokemon.name} style={{ backgroundColor: getTypeColor(type) }}>
+                    <li key={pokemon.name}>
                         <Link to={`/pokemon/${pokemon.name}`}>{pokemon.name}</Link>
                        
                     </li>
