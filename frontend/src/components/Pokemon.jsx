0e3ce4e01e3472df7a8dccfbd911659a1274b04f
Samatara <samatarbulbula@gmail.com>
2024-05-20 11:09:22 +0200

    fetching av abilitiy
    jeg har hentet mer data en vi hadde tidligere. jeg har fetchet effect og short_effect på pokemonenene.

--- frontend/src/components/Pokemon.jsx
+++ frontend/src/components/Pokemon.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 const Pokemon = () => {
   const { name } = useParams();
   const [pokemonData, setPokemonData] = useState(null);
+  const [abilityDetails, setAbilityDetails] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -12,6 +13,16 @@ const Pokemon = () => {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
         const data = await response.json();
         setPokemonData(data);
+
+
+        const abilities = data.abilities.map(async (abilityInfo) => {
+          const abilityResponse = await fetch(abilityInfo.ability.url);
+          return abilityResponse.json();
+        });
+
+        const abilitiesData = await Promise.all(abilities);
+        setAbilityDetails(abilitiesData);
+
       } catch (error) {
         console.error("Error fetching data:", error);
       } finally {
@@ -37,7 +48,16 @@ const Pokemon = () => {
       <h1>{pokemonData.name}</h1>
       <p>Types: {pokemonData.types.map(t => t.type.name).join(', ')}</p>
       <p>Stats: {pokemonData.stats.map(s => `${s.stat.name}: ${s.base_stat}`).join(', ')}</p>
-      <p>Abilities: {pokemonData.abilities.map(a => a.ability.name).join(', ')}</p>
+      <div>
+          <h2>Abilities</h2>
+          {abilityDetails.map((ability, index) => (
+            <div key={index}>
+              <h3>{ability.name}</h3>
+              <p><strong>Effect:</strong>{ability.effect_entries.find(entry => entry.language.name === 'en').effect}</p>
+              <p><strong>Short effect:</strong> {ability.effect_entries.find(entry => entry.language.name === 'en').short_effect}</p>
+          </div>
+          ))}
+        </div>
       <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
     </section>
     </main>
